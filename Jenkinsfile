3c7bdcde70fa3526e143262c264fcac881012ed7
mohamed nebbai <nebbai1999@gmail.com>
2023-04-23 20:33:49 +0100

    v1.0

---
+++ Jenkinsfile
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+
+    environment {
+        AWS_DEFAULT_REGION ='us-east-1'
+        PROJECT_NAME = 'ecs-service-test'
+    }
+
+    stages {
+        
+        stage('Install Dependencies') {
+            steps {
+                sh 'npm install aws-cdk-lib'
+            }
+        }
+
+        stage('Bootstrap') {
+            steps {
+                sh 'cdk bootstrap'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                withAWS(credentials: 'aws-access-key-id', region: 'us-east-1') {
+                    sh 'cdk deploy --require-approval=never'
+                }
+            }
+        }
+    }
+}
--- lib/frontend-stack.ts
+++ lib/frontend-stack.ts
@@ -1,16 +1,49 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnCloudFrontOriginAccessIdentity, CfnOriginAccessControl, CloudFrontAllowedCachedMethods, CloudFrontAllowedMethods, CloudFrontWebDistribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import path = require('path');
+
 
 export class FrontendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, "Bucket", {
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "error.html",
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FrontendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const deployment = new BucketDeployment(this, 'ReactAppDeployment', {
+      sources: [Source.asset("./build")], // path to your React app build directory
+      destinationBucket: bucket,
+
+    });
+
+    const oai = new OriginAccessIdentity(this, "oai");
+
+    const cdn = new CloudFrontWebDistribution(this, 'CloudFrontDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: oai
+          },
+          behaviors: [{ isDefaultBehavior: true }],
+        },
+      ],
+      defaultRootObject: 'index.html'
+    });
+
+    const cloudfrontAccessPolicy = new PolicyStatement();
+    cloudfrontAccessPolicy.addActions("s3:GetObject");
+    cloudfrontAccessPolicy.addPrincipals(oai.grantPrincipal);
+    cloudfrontAccessPolicy.addResources(bucket.arnForObjects("*"));
+    bucket.addToResourcePolicy(cloudfrontAccessPolicy);
+
+
   }
 }
